f9293fe20088fa4eea81f0db1131cebb6c307b5b
Avani Ghetia <Avani@Avanis-MacBook-Pro.local>
2016-08-24 15:47:44 -0400

    Hangman newgit add -A

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,60 @@
+var teamArray = ["eagles","giants","redskins","cowboys","patriots","dolphins","bills","jets","packers","vikings","bears","lions","colts","jaguars","texans","titans","panthers","buccaneers","saints","falcons","seahawks","cardinals","49ers","rams","ravens","browns","bengals","steelers","chiefs","broncos","raiders","chargers"];
+// whatever we used in the html to define this id
+var userGuessedLetters = "User Guessed Letters :";  
+var wrounglettercount= 1;
+var word;
+var numberofgussedletter=8;
+// var numLives= 10;
 
-var word = "";
-var currentWord = "";
-var guesses = 0;
-
-
-function initialize() {
-	var opponentChoice;
-	var playerWins = 0;
-	var playerLosses = 0;
-	var teamArray = ["eagles","giants","redskins","cowboys","patriots","dolphins","bills","jets","packers","vikings","bears","lions","colts","jaguars","texans","titans","panthers","buccaneers","saints","falcons","seahawks","cardinals","49ers","rams","ravens","browns","bengals","steelers","chiefs","broncos","raiders","chargers"];
-
-	randomNumber = teamArray[Math.floor(Math.random() * teamArray.length)];
-	currentWord = teamArray[randomNumber];
-	console.log(currentWord);
-	for (var i = 0; i < currentWord.length; i++) {
-		j = i;
-		while (j == 0) {
-			word = "";
-			j++;
-		}
-		if (currentWord[i] == " ") {
-			word += " ";	
-		}
-		word += "_";
-	}
-
-	document.getElementById("wordstatus").innerHTML	= word;
-	console.log(see);
-}
-
-
-document.onkeydown = function(event) {
-
-	var WordBeingPlayed = "";
-	var correctGuess = false;
-	Played = String.fromCharCode(event.keyCode).toLowerCase();
-	for (k = 0; k < currentWord.length; k++) {
-		if (Played == currentWord[k]) {
-			WordBeingPlayed += Played;
-			console.log("Match! tempWordBeingPlayed is " + WordBeingPlayed);
-			correctGuess = true;
-		}else if (word[k] != "_") {
-			WordBeingPlayed += word[k];
-			console.log("Already selected! tempWordBeingPlayed is " + WordBeingPlayed);
-		}else {
-			WordBeingPlayed += "_";
-			console.log("Blank space! tempWordBeingPlayed is " + WordBeingPlayed);
-		}
-		document.getElementById("wordstatus").innerHTML	= WordBeingPlayed;
-
-	}
-	word = WordBeingPlayed;
-	if (matchedGuess == true) {
-		wordComplete();
-	}
-	
-	if (matchedGuess == false) {
-		guesses++;
-		document.getElementById("source-button").innerHTML = guesses;
-		loseGame();	
-	}
-	
-}
-
-function wordComplete() {
+word = teamArray[Math.floor(Math.random()* teamArray.length)];
+var newteam=new Array(word.length);
+// this gives a number of dashees to the line
+for(var j=0; j<word.length; j++){
+  newteam[j] = '_';
+  // console.log('correct');
 
 }
 
-function loseGame() {
-	
+
+var teamMessage= "Team: " + newteam;
+document.getElementById("teamName").innerHTML = teamMessage;
+// press key
+document.onkeyup = function(event){
+  // convert letters into lower case
+  var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  var guess=false;
+  for(var i=0; i<word.length; i++){
+    if(word[i]==userguess){
+      newteam[i]=userguess;
+      guess = true;
+    }
+  }
+  if(!guess){
+    numberofgussedletter = numberofgussedletter-1;
+    userGuessedLetters= userGuessedLetters+userguess+" ";
+  }
+  var myteam = "Team: " + newteam;
+  document.getElementById("teamName").innerHTML= myteam;
+
+  var numLivestMessage = "Lives Left: "+numberofgussedletter;
+  document.getElementById("numlives").innerHTML= numLivestMessage;
+  document.getElementById("userGuessedLetters").innerHTML= userGuessedLetters; 
+  if(numlives==0){
+    alert('ohhh!! SORRY!! You almost got Hanged! Lets play again!!');
+    location.reload(true);
+  }
+  var correctLetter=true;
+ for(var i= 0; i<word.length;i++){
+     if(newteam[i]=="_"){
+       correctLetter=false;
+     }
+}
+if(correctLetter){
+       setTimeout(function(){
+        alert(' You Won!!! Keep playing! '); 
+         location.reload(true);   
+       }, 1000);
+     }
+       
 }
